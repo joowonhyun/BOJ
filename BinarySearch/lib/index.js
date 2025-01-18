64ca8611577883c8fd3d7cfce861c0a9de617563
joowonhyun <78545341+joowonhyun@users.noreply.github.com>
2025-01-18 22:41:20 +0900

    fix: 틀린 주석 수정

--- BinarySearch/lib/index.js
+++ BinarySearch/lib/index.js
@@ -9,7 +9,7 @@ function lowerBound(arr, target, start, end) {
   return end;
 }
 
-// 정렬된 순서를 유지하면서 배열에 삽입할 가장 왼쪽 인덱스 반환
+// 정렬된 순서를 유지하면서 배열에 삽입할 가장 오른쪽 인덱스 반환
 function upperBound(arr, target, start, end) {
   // 배열에 삽입할 수 있는 가장 왼쪽 인덱스를 찾기 위해 start가 end보다 작을때에만 수행되는 것이 특징
   // (start가 end와 같아지면 더 이상 탐색할 필요가 없는 상태)
