e2471a5358a33093ac7111a9c48d2242f7aa10f9
joowonhyun <78545341+joowonhyun@users.noreply.github.com>
2025-02-01 22:12:58 +0900

    refactor: BOJ10971

--- BackTracking/BOJ10971/input.js
+++ BackTracking/BOJ10971/input.js
@@ -7,26 +7,25 @@ const input = fs
 const n = parseInt(input[0]); // 첫 번째 줄에서 n을 숫자로 변환
 const w = input.slice(1).map((line) => line.split(" ").map(Number));
 
-let minCost = Number.MAX_VALUE; // 초기값을 최대값으로 설정
-const visited = new Array(n).fill(false); // 방문 여부 배열 초기화
+let MIN_CONST = Number.MAX_VALUE;
+const visited = new Array(n).fill(false);
 
 function backtrack(currentCity, count, cost) {
   if (count === n) {
     if (w[currentCity][0] > 0) {
-      minCost = Math.min(minCost, cost + w[currentCity][0]);
+      MIN_CONST = Math.min(MIN_CONST, cost + w[currentCity][0]); // 누적 비용 + 시작지점으로 돌아가는 비용
     }
     return;
   }
 
-  for (let nextCity = 0; nextCity < n; nextCity++) {
-    if (w[currentCity][nextCity] > 0 && !visited[nextCity]) {
-      visited[nextCity] = true; // 도시 방문 표시
-      backtrack(nextCity, count + 1, cost + w[currentCity][nextCity]); // 재귀 호출
-      visited[nextCity] = false; // 백트래킹
+  for (let i = 0; i < n; i++) {
+    if (w[currentCity][i] > 0 && !visited[i]) {
+      visited[i] = true;
+      backtrack(i, count + 1, cost + w[currentCity][i]);
+      visited[i] = false;
     }
   }
 }
-
 visited[0] = true; // 시작 도시 방문
 backtrack(0, 1, 0); // 백트래킹 시작
-console.log(minCost); // 최소 비용 반환
+console.log(MIN_CONST); // 최소 비용 반환
