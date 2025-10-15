4e5f0206252b0a6fe0f629f4f0647c1fea242920
Joowon Hyun <78545341+joowonhyun@users.noreply.github.com>
2025-10-16 00:36:27 +0900

    refactor: 변수명 수정

--- DFS/1240/input.js
+++ DFS/1240/input.js
@@ -6,32 +6,27 @@ const input = fs
   .split("\n");
 
 let index = 1;
-const [N, M] = input[0].split(" ").map(Number); // 노드 개수 N, 쌍의 개수 M
-
+const [N, M] = input[0].split(" ").map(Number);
 const graph = Array.from({ length: N + 1 }, () => []);
 let distance = Array(N + 1).fill(-1);
 
 for (let i = 0; i < N - 1; i++) {
-  const [x, y, cost] = input[index++].split(" ").map(Number);
-  graph[x].push([y, cost]);
-  graph[y].push([x, cost]);
+  const [a, b, dist] = input[index++].split(" ").map(Number);
+  graph[a].push([b, dist]);
+  graph[b].push([a, dist]);
 }
 
-function dfs(x, dist) {
-  // 아직 방문하지 않은 경우만 처리
-  if (distance[x] !== -1) return;
-
-  distance[x] = dist;
-
-  for (let [y, cost] of graph[x]) {
+function dfs(node, dist) {
+  if (distance[node] !== -1) return;
+  distance[node] = dist;
+  for (const [y, cost] of graph[node]) {
     dfs(y, dist + cost);
   }
 }
 
 for (let i = 0; i < M; i++) {
-  const [x, y] = input[index++].split(" ").map(Number);
-
-  distance.fill(-1); // 초기화 (방문하지 않음을 -1로 표시)
-  dfs(x, 0); // x에서 시작해 거리 계산
-  console.log(distance[y]);
+  const [start, end] = input[index++].split(" ").map(Number);
+  distance.fill(-1);
+  dfs(start, 0);
+  console.log(distance[end]);
 }
